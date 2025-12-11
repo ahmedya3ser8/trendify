d5c5c6b70dcc4dfc1e9b6c4b673f32295a8c18b8
ahmed yasser <ahmedya3ser8@gmail.com>
2025-12-11 11:40:03 +0200

    ADD: new-collection component in home page

--- src/features/main/pages/home/components/Hero.tsx
+++ src/features/main/pages/home/components/Hero.tsx
@@ -7,7 +7,9 @@ const Hero = () => {
     <section className={`pt-24 ${styles.hero_bg}`}>
       <div className="container">
         <div className="hero_content flex flex-col gap-6 text-white">
-          <h1 className='text-3xl sm:text-4xl lg:text-5xl font-semibold'> Elevate Your Home <br /> with Stylish Furniture </h1>
+          <h1 className='text-3xl sm:text-4xl lg:text-5xl font-semibold leading-10 md:leading-14 lg:leading-16'> 
+            Elevate Your Home <br /> with Stylish Furniture 
+          </h1>
           <p className='w-full text-sm md:text-base sm:w-lg md:w-xl'> 
             Explore a range of high-quality, affordable furniture designed to transform any room. 
             Discover the perfect blend of style, comfort, and functionality for your space. 
--- src/features/main/pages/home/components/NewCollection.tsx
+++ src/features/main/pages/home/components/NewCollection.tsx
@@ -1,7 +1,40 @@
+import collectionImg1 from '@/assets/images/home/collections/collection-1.png';
+import collectionImg2 from '@/assets/images/home/collections/collection-2.webp';
+import collectionImg3 from '@/assets/images/home/collections/collection-3.webp';
+import MainTitle from '@/features/main/components/MainTitle';
 
 const NewCollection = () => {
   return (
-    <div>NewCollection</div>
+    <section className="py-10">
+      <div className="container">
+        <MainTitle title='New Collection' description='Check the new Collection' />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="col h-[300px] lg:h-[500px] relative rounded-lg overflow-hidden">
+            <img src={collectionImg1} className='w-full h-full object-cover' alt="collection_image" />
+            <div className="collection_content absolute inset-0 bg-black/30 text-white flex flex-col gap-3 justify-center items-center">
+              <h4 className='text-3xl'>Shoes</h4>
+              <p className='text-3xl'>up to 50% off</p>
+            </div>
+          </div>
+          <div className="col space-y-6 md:space-y-4">
+            <div className="col h-[300px] md:h-[142px] lg:h-[242px] relative rounded-lg overflow-hidden">
+              <img src={collectionImg2} className='w-full h-full object-cover' alt="collection_image" />
+              <div className="collection_content absolute inset-0 bg-black/30 text-white flex flex-col gap-3 justify-center items-center">
+                <h4 className='text-3xl'>Bags</h4>
+                <p className='text-3xl'>up to 50% off</p>
+              </div>
+            </div>
+            <div className="col h-[300px] md:h-[142px] lg:h-[242px] relative rounded-lg overflow-hidden">
+              <img src={collectionImg3} className='w-full h-full object-cover' alt="collection_image" />
+              <div className="collection_content absolute inset-0 bg-black/30 text-white flex flex-col gap-3 justify-center items-center">
+                <h4 className='text-3xl'>Accessories</h4>
+                <p className='text-3xl'>up to 50% off</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
